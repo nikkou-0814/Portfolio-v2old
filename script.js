ab3b41ec3426846c17aedb7893f973dc6883aaf3
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2023-09-28 22:08:58 +0900

    ^^

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function close_modal(){
     document.getElementById("hello_modal").style.display="none";
     }
+
+function topButton() {
+
+    duration=100;
+    var begin = new Date() - 0;
+    var yOffset = window.pageYOffset;
+    var timer= setInterval(function() {
+        var current = new Date() - begin;
+        if (current > duration) {
+            clearInterval(timer);
+            current = duration;
+        }
+
+        //スクロール位置を単位時間で変更する
+        window.scrollTo(0, yOffset * (1 - current / duration)) 
+    }, 10);
+
+}
